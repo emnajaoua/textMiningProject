dea85cb10f97b98154c701f2ae13b8861f695970
emna.jaoua <jaoua01@gw.uni-passau.de>
2018-02-16 20:04:14 +0100

    add preprocessing and similarity

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import gzip
+import re
 from pycorenlp import StanfordCoreNLP
+from stanfordcorenlp import StanfordCoreNLP
 
-nlp = StanfordCoreNLP('http://localhost:9000')
 
 def parse(path):
     g = gzip.open(path, 'rb')
@@ -27,13 +28,52 @@ def writePDinList():
     return reviewTextList, summaryList
 
 list1, list2 = writePDinList()
-print('test', writePDinList()[1])
-text = (
-  'Pusheen and Smitha walked along the beach. '
-  'Pusheen wanted to surf, but fell off the surfboard.')
-output = nlp.annotate(text, properties={
-  'annotators': 'tokenize,ssplit,pos,depparse,parse',
-  'outputFormat': 'json'
-  })
-print(output['sentences'][0]['parse'])
+# print('test', writePDinList()[1])
+# ##get constituency parser
+# nlp = StanfordCoreNLP('http://localhost:9000')
+#
+# text = (
+#   'Pusheen wanted to surf, but fell off the surfboard.')
+# output = nlp.annotate(text, properties={
+#   'annotators': 'tokenize,ssplit,pos,depparse,parse',
+#   'outputFormat': 'json'
+#   })
+# print(output['sentences'][0]['parse'])
+def parseSentence(sentence):
+    nlp = StanfordCoreNLP( r'/root/PycharmProjects/textmining/textmining/textMiningProject/stanford-corenlp-full-2018-01-31')
 
+    # sentence = 'Guangdong University of Foreign Studies is located in Guangzhou.'
+    print('Tokenize:', nlp.word_tokenize(sentence))
+    print('Part of Speech:', nlp.pos_tag(sentence))
+    print('Named Entities:', nlp.ner(sentence))
+    print('Constituency Parsing:', nlp.parse(sentence))
+    print('Dependency Parsing:', nlp.dependency_parse(sentence))
+    nlp.close() # Do not forget to close! The backend server will consume a lot memery.
+    return nlp.dependency_parse(sentence)
+
+def dependency_parse(sentence):
+    """ Accepts a sentence and returns its dependency parse as a list-of-lists
+    Also returns the list of nouns in the sentence
+    """
+    parse_output = parseSentence(sentence)
+
+    # List of nouns
+    const_parse = parse_output[0]
+    print(const_parse)
+    regex_pattern = r"\(NN (\w+)\)"
+    NN_list = re.findall(r"\(NN (\w+)\)", const_parse)
+    NNS_list = re.findall(r"\(NNS (\w+)\)", const_parse)
+
+    noun_list = NN_list + NNS_list
+
+    # Dependency parse
+    dep_parse = parse_output[1].split("\n")
+
+    print("---")
+    dependency_parse=[]
+    for i in dep_parse:
+        if len(i.strip()) > 0 and i.strip()[0] != "(":
+            line=i.strip()
+            dependency_parse.append(filter(lambda x:x.isalpha(),re.findall(r"[\w']+", line)))
+
+    return dependency_parse, noun_list
